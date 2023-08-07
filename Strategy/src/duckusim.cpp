0e176eac9c65921c50a0260159ae9b892f389832
seutil <seutil@inbox.ru>
2023-08-08 00:19:46 +0300

    main

---
+++ Strategy/src/duckusim.cpp
@@ -0,0 +1,18 @@
+#include <ducks.hpp>
+
+int main()
+{
+    using namespace DuckUSim;
+
+    std::unique_ptr<Duck> duck = std::make_unique<MallardDuck>();
+    duck->fly();
+    duck->swim();
+    duck->quack();
+
+    duck = std::make_unique<ModelDuck>();
+    duck->fly();
+    duck->swim();
+    duck->quack();
+
+    return 0;
+}
