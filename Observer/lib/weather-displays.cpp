f8c1fb4e76ea8f85af49882076bb1c9de17e7e6f
seutil <seutil@inbox.ru>
2023-08-09 16:15:09 +0300

    Weather displays

---
+++ Observer/lib/weather-displays.cpp
@@ -0,0 +1,31 @@
+#include <numeric>
+#include <iostream>
+#include "weather-displays.hpp"
+using namespace WeatherStation;
+
+void
+CurrentWeatherDisplay::update(const double& temperature,
+                              const double& humidity,
+                              const double& pressure)
+{
+    std::cout << "====== CURRENT DISPLAY =====\n"
+              << "Temperature: " << temperature << "\n"
+              << "Humidity:    " << humidity << "\n"
+              << "Pressure:    " << pressure << "\n"
+              << "============================\n\n";
+}
+
+void
+StaticticsWeatherDisplay::update(const double& temperature,
+                                 const double& humidity,
+                                 const double& pressure)
+{
+    m_temperatures.push_back(temperature);
+    m_humidities.push_back(humidity);
+    m_pressures.push_back(pressure);
+    std::cout << "===== STATISTICS DISPLAY =====\n"
+              << "Average temperatue: " << std::accumulate(m_temperatures.begin(), m_temperatures.end(), 0.0) / m_temperatures.size() << "\n"
+              << "Average humidity:   " << std::accumulate(m_humidities.begin(), m_humidities.end(), 0.0) / m_humidities.size() << "\n"
+              << "Average pressure:   " << std::accumulate(m_pressures.begin(), m_pressures.end(), 0.0) / m_pressures.size() << "\n"
+              << "==============================\n\n";
+}
---
+++ Observer/lib/weather-displays.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <vector>
+
+namespace WeatherStation
+{
+
+class IWeatherDisplay
+{
+public:
+    virtual void update(const double& temperature,
+                        const double& humidity,
+                        const double& pressure) = 0;
+    virtual ~IWeatherDisplay() = default;
+};
+
+class CurrentWeatherDisplay : public IWeatherDisplay
+{
+public:
+    void update(const double& temperture,
+                const double& humidity,
+                const double& pressure) override;
+};
+
+class StaticticsWeatherDisplay : public IWeatherDisplay
+{
+public:
+    void update(const double& temperatue,
+                const double& humidity,
+                const double& pressure) override;
+protected:
+    std::vector<double> m_temperatures;
+    std::vector<double> m_humidities;
+    std::vector<double> m_pressures;
+};
+
+}
