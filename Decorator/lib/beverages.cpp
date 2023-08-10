6259c05bd0ffc1e736e7fe041418be019071d4d5
seutil <seutil@inbox.ru>
2023-08-10 08:14:34 +0300

    Beverage classes

---
+++ Decorator/lib/beverages.cpp
@@ -0,0 +1,50 @@
+#include "beverages.hpp"
+using namespace Starbuzz;
+
+Beverage::Beverage(const std::string& name,
+                   const double& cost,
+                   const Size& size)
+    : m_name{name},
+      m_cost{cost},
+      m_size{size}
+{}
+
+const std::string&
+Beverage::name() const
+{
+    return m_name;
+}
+
+const double&
+Beverage::cost() const
+{
+    return m_cost;
+}
+
+const Beverage::Size&
+Beverage::size() const
+{
+    return m_size;
+}
+
+void
+Beverage::set_size(const Size& size)
+{
+    m_size = size;
+}
+
+HouseBlend::HouseBlend(const Size& size)
+    : Beverage("House Blend Coffee", .89, size)
+{}
+
+DarkRoast::DarkRoast(const Size& size)
+    : Beverage("Dark Roast Coffee", .99, size)
+{}
+
+Decaf::Decaf(const Size& size)
+    : Beverage("Decaf Coffee", 1.05, size)
+{}
+
+Espresso::Espresso(const Size& size)
+    : Beverage("Epsresso Coffee", 1.99, size)
+{}
---
+++ Decorator/lib/beverages.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <string>
+
+namespace Starbuzz
+{
+
+class Beverage
+{
+public:
+    enum class Size
+    {
+        SHORT,
+        TALL,
+        GRANDE
+    };
+
+    const std::string& name() const;
+    const double& cost() const;
+    const Size& size() const;
+    void set_size(const Size& size);
+    virtual ~Beverage() = default;
+protected:
+    Beverage(const std::string& name,
+             const double& cost,
+             const Size& size);
+
+    std::string m_name;
+    double m_cost;
+    Size m_size;
+};
+
+class HouseBlend : public Beverage
+{
+public:
+    HouseBlend(const Size& size = Size::TALL);
+};
+
+class DarkRoast : public Beverage
+{
+public:
+    DarkRoast(const Size& size = Size::TALL);
+};
+
+class Decaf : public Beverage
+{
+public:
+    Decaf(const Size& size = Size::TALL);
+};
+
+class Espresso : public Beverage
+{
+public:
+    Espresso(const Size& size = Size::TALL);
+};
+
+}
