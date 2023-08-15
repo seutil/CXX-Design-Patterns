0ce5685662b2938c5b66bc7ce3643471d8377a7f
seutil <seutil@inbox.ru>
2023-08-15 10:09:20 +0300

    Plug classes

---
+++ Adapter/lib/plugs.cpp
@@ -0,0 +1,44 @@
+#include <assert.h>
+#include <iostream>
+#include "plugs.hpp"
+using namespace Adapter;
+
+void
+UsaPlug::connect(std::shared_ptr<PowerSocket> power_socket)
+{
+    m_power_socket = std::dynamic_pointer_cast<UsaPowerSocket>(power_socket);
+    assert(!m_power_socket.expired());
+    std::cout << "Plug to " << power_socket->name() << " socket\n";
+}
+
+void
+UsaPlug::disconnect()
+{
+    if (m_power_socket.expired())
+    {
+        std::cout << "Already disconnected\n";
+        return;
+    }
+
+    std::cout << "Disconnecting...\n";
+    m_power_socket.reset();
+}
+
+void
+EuropePlug::connect(std::shared_ptr<PowerSocket> power_socket)
+{
+    m_power_socket = std::dynamic_pointer_cast<EuropePowerSocket>(power_socket);
+}
+
+void
+EuropePlug::disconnect()
+{
+    if (m_power_socket.expired())
+    {
+        std::cout << "Already disconnected\n";
+        return;
+    }
+
+    std::cout << "Disconnecting...\n";
+    m_power_socket.reset();
+}
---
+++ Adapter/lib/plugs.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <memory>
+#include "power-sockets.hpp"
+
+namespace Adapter
+{
+
+class IPlug
+{
+public:
+    virtual void connect(std::shared_ptr<PowerSocket> power_socket) = 0;
+    virtual void disconnect() = 0;
+    virtual ~IPlug() = default;
+};
+
+class UsaPlug : public IPlug
+{
+public:
+    void connect(std::shared_ptr<PowerSocket> power_socket) override;
+    void disconnect() override;
+protected:
+    std::weak_ptr<UsaPowerSocket> m_power_socket;
+};
+
+class EuropePlug : public IPlug
+{
+public:
+    void connect(std::shared_ptr<PowerSocket> power_socket) override;
+    void disconnect() override;
+protected:
+    std::weak_ptr<EuropePowerSocket> m_power_socket;
+};
+
+}
