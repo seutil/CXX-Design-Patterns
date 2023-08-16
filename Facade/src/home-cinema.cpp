a275dea63c93f17a51c60592ae78c8698753de67
seutil <seutil@inbox.ru>
2023-08-16 21:20:25 +0300

    Home cinema executable

---
+++ Facade/src/home-cinema.cpp
@@ -0,0 +1,10 @@
+#include <components.hpp>
+
+int main()
+{
+    using namespace HomeCinema;
+    std::unique_ptr home_cinema = std::make_unique<CinemaFacade>();
+    home_cinema->on();
+    home_cinema->off();
+    return 0;
+}
